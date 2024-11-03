7a713cd85bbdd822bdf37c9a501eb65649889c88
yosra2001 <ouesletyyosra@gmail.com>
2024-11-03 17:39:14 +0100

    Add unit tests for Department class and update Jenkins pipeline 9

--- backendPidev/src/test/java/com/pidev/backend/Entity/DepartmentTest.java
+++ backendPidev/src/test/java/com/pidev/backend/Entity/DepartmentTest.java
@@ -2,9 +2,11 @@ package com.pidev.backend.Entity;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,6 +14,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.mockito.Mockito.mock;
+@RunWith(MockitoJUnitRunner.class)
 
 public class DepartmentTest {
 
