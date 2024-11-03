2e65455f2ee89e8d977c68c1ba469342fd06167c
yosra2001 <ouesletyyosra@gmail.com>
2024-11-03 18:24:29 +0100

    Add unit tests for Department class and update Jenkins pipeline 10

---
+++ backendPidev/src/test/java/com/pidev/backend/Entity/SimpleTest.java
@@ -0,0 +1,11 @@
+package com.pidev.backend.Entity;
+
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleTest {
+    @Test
+    public void simpleTest() {
+        assertTrue(true);
+    }
+}
