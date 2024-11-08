1e81683c9536c8c532a7c97604e4619e91f5812c
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:25:44 +0100

    Update FileDBRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/FileDBRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/FileDBRepository.java
@@ -2,12 +2,12 @@ package com.pidev.backend.Repository;
 
 
 import com.pidev.backend.Entity.FileDB;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface FileDBRepository extends MongoRepository<FileDB, String> {
+public interface FileDBRepository extends JpaRepository<FileDB, String> {
 	
 
 }
