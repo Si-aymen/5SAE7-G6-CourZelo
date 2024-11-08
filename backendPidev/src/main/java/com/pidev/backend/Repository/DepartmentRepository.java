d102969925381de796bf3bf7a997865a8a9f89ba
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:25:23 +0100

    Update DepartmentRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/DepartmentRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/DepartmentRepository.java
@@ -1,11 +1,11 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Department;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DepartmentRepository extends MongoRepository<Department, String> {
+public interface DepartmentRepository extends JpaRepository<Department, String> {
 
     Department findDepartmentByDepartmentId(String id);
 }
