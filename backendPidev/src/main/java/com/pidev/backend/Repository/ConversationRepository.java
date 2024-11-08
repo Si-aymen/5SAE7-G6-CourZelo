6f2d39fd199b79201aa0f547346607ba9c9788c7
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:24:59 +0100

    Update ConversationRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/ConversationRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/ConversationRepository.java
@@ -1,9 +1,9 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Conversation;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ConversationRepository extends MongoRepository<Conversation,String> {
+public interface ConversationRepository extends JpaRepository<Conversation,String> {
 }
