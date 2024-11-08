40b65a4e0c19459c8e6588ba6536c35055001fd3
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:27:13 +0100

    Update ReclamationRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/ReclamationRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/ReclamationRepository.java
@@ -1,12 +1,13 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Reclamation;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ReclamationRepository extends MongoRepository<Reclamation, String> {
+public interface ReclamationRepository extends JpaRepository<Reclamation, String> {
     List<Reclamation> findByUser_Id(String userId);
 }
