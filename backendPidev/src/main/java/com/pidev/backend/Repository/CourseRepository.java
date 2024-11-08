3560e80b0211ca8adc648949d02c8f1faa3ef704
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:17:16 +0100

    Update CourseRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/CourseRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/CourseRepository.java
@@ -2,12 +2,12 @@ package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Course;
 import com.pidev.backend.Entity.Speciality;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CourseRepository extends MongoRepository<Course, String> {
+public interface CourseRepository extends JpaRepository<Course, String> {
     List<Course> getCourseByCourseDomain(Speciality speciality);
 }
