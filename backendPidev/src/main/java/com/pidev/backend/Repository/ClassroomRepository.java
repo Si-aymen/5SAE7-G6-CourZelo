95fe7326feca6a38f33e666eb11b3b86c9fb5547
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:24:30 +0100

    Update ClassroomRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/ClassroomRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/ClassroomRepository.java
@@ -2,13 +2,13 @@ package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Classroom;
 import com.pidev.backend.Entity.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ClassroomRepository extends MongoRepository<Classroom, String> {
+public interface ClassroomRepository extends JpaRepository<Classroom, String> {
     Classroom getClassroomByClassroomName(String lvl );
     Classroom getClassroomById(String id) ;
 
