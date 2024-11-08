aec7beb2f51e6a4f33202c8713d1c7464c227a66
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:26:54 +0100

    Update QuestionRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/QuestionRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/QuestionRepository.java
@@ -1,12 +1,12 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Question;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface QuestionRepository extends MongoRepository<Question, String> {
+public interface QuestionRepository extends JpaRepository<Question, String> {
     List<Question> findByContenue(String contenue);//rech
 }
