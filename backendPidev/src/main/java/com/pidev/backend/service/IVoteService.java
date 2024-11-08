cdde8b0b7d51f7cc8d93c5ddf76c1322104df78a
Si-aymen <rahali.aymen2001@gmail.com>
2024-11-08 12:04:21 +0100

    correct Vote Service

--- backendPidev/src/main/java/com/pidev/backend/service/IVoteService.java
+++ backendPidev/src/main/java/com/pidev/backend/service/IVoteService.java
@@ -1,9 +1,7 @@
 package com.pidev.backend.service;
 
-import com.pidev.backend.entity.Vote;
 
 public interface IVoteService {
-   //public Vote ajoutdeletevote(Vote v, String idu , String idq);
     public int nbvotebyquest(String idq);
 
 }
--- backendPidev/src/main/java/com/pidev/backend/serviceImpl/VoteServiceImpl.java
+++ backendPidev/src/main/java/com/pidev/backend/serviceImpl/VoteServiceImpl.java
@@ -1,8 +1,7 @@
 package com.pidev.backend.serviceImpl;
 
 import com.pidev.backend.entity.Question;
-import com.pidev.backend.entity.User;
-import com.pidev.backend.entity.Vote;
+
 import com.pidev.backend.repository.QuestionRepository;
 import com.pidev.backend.repository.UserRepository;
 import com.pidev.backend.repository.VoteRepository;
@@ -18,78 +17,6 @@ public class VoteServiceImpl implements IVoteService {
     VoteRepository voterepo;
     @Autowired
     QuestionRepository questionrepo;
-    public Vote ajoutdeletevote(Vote v, String idu, String idq) {
-       /* Question question = questionrepo.findById(idq).orElse(null);
-        User user = userrepo.findById(idu).orElse(null);
-        if (user != null && question != null) {
-            Vote vote = voterepo.findByQuestionAndUser(idq, idu);
-            if (vote != null ) {
-                if(user.getVotes() != null && question.getVotes() != null){
-                    // Remove the vote from user's votes list
-
-                   for(Vote vo :user.getVotes()){
-                       if(vo.equals(vote)){
-                           user.getVotes().remove(vo);
-                           userrepo.save(user);
-                           break;
-                       }
-                   }
-
-
-                    for(Vote vo :question.getVotes()){
-                        if(vo.equals(vote)){
-                            question.getVotes().remove(vo);
-                            System.out.println(question.getVotes().size());
-                            questionrepo.save(question);
-                            break;
-                        }
-                    }
-
-                    // Remove the vote from question's votes list
-
-
-                    // Remove the vote entity
-                    voterepo.delete(vote);
-                }
-                return vote;
-            } else {
-                voterepo.save(v);
-                if(user.getVotes() != null){
-                    user.getVotes().add(v);
-                    userrepo.save(user);
-                }else{
-                    user.getVotes().add(v);
-                    userrepo.save(user);
-                }
-                if(question.getVotes() != null){
-                    question.getVotes().add(v);
-                    questionrepo.save(question);
-                }else{
-                    question.getVotes().add(v);
-                    questionrepo.save(question);
-                }
-
-
-
-
-                v.setUser(user);
-                v.setQuestion(question);
-
-                // Save the new vote entity
-                return voterepo.save(v);
-
-            }
-
-        }
-        return v;
-*/
-        return null;
-    }
-
-
-
-
-
 
     @Override
     public int nbvotebyquest(String idq) {
