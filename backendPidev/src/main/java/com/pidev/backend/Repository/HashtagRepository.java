467e9902e078e0fa3c086d0c456745e6419386e2
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:26:16 +0100

    Update HashtagRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/HashtagRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/HashtagRepository.java
@@ -1,9 +1,9 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Hashtag;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface HashtagRepository extends MongoRepository<Hashtag, String> {
+public interface HashtagRepository extends JpaRepository<Hashtag, String> {
 }
