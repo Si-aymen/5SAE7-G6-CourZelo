711fe3ef8a70ef8fc002df1d364712521b9d13ee
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:26:03 +0100

    Update GlossaryRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/GlossaryRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/GlossaryRepository.java
@@ -1,10 +1,10 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.Glossary;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 @Repository
-public interface GlossaryRepository extends MongoRepository<Glossary, String> {
+public interface GlossaryRepository extends JpaRepository<Glossary, String> {
 
 
      //Flux<Glossary> findByTermContainingIgnoreCase(String term);
