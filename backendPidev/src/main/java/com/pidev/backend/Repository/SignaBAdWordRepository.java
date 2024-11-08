7b9342bf2489788f321925fc1dcd0e81a9eb2b1a
nourchaine77 <125187887+nourchaine77@users.noreply.github.com>
2024-11-08 15:27:54 +0100

    Update SignaBAdWordRepository.java

--- backendPidev/src/main/java/com/pidev/backend/Repository/SignaBAdWordRepository.java
+++ backendPidev/src/main/java/com/pidev/backend/Repository/SignaBAdWordRepository.java
@@ -1,9 +1,9 @@
 package com.pidev.backend.Repository;
 
 import com.pidev.backend.Entity.SignalBadword;
-import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SignaBAdWordRepository extends MongoRepository<SignalBadword, String> {
+public interface SignaBAdWordRepository extends JpaRepository<SignalBadword, String> {
 }
